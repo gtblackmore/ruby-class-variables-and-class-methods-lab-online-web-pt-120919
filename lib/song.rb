5a87ec818c07c38ddccea70b027f2f4fff7c1e76
George Blackmore <gtblackmore@gmail.com>
2019-12-23 01:21:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,9 @@ class Song
   @@genres = []
   
   def initialize(name, artist, genre)
-    @name
-    @artist
-    @genre
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
   end
   
