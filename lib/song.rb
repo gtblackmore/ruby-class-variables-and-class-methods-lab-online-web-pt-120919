2eb7598818dc0be439c252b513740f6c8734b6cd
George Blackmore <gtblackmore@gmail.com>
2019-12-23 01:23:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ class Song
   
   @@count = 0
   @@genres = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @name = name
@@ -18,6 +19,12 @@ class Song
   def self.genres(genre)
     if @@genre.includes?(genre) == false
       @@genre << genre
+    end
+  end
+  
+  def self.artists(artist)
+    if @@artists.includes?(artist) == false
+      @@artists << artist
     end
   end
       
