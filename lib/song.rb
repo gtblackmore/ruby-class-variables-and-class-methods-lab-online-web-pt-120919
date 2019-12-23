1832e25e66d494a85e60dd514690e755ae819c52
George Blackmore <gtblackmore@gmail.com>
2019-12-23 01:37:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,9 @@ class Song
   def genre_count
     genre_count = {}
     @@genres.each do |genre|
-       
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
       
 end
